0047999a9eb01a3f7c61c365fc05699f1c2f722b
Monchol <64288365+Monchol@users.noreply.github.com>
2023-02-17 19:17:34 -0300

    Improve on the code

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const background = document.querySelector(".background");
 
 sidebarButton.forEach((sidebarButton) => {
     sidebarButton.addEventListener("click", function(){
-        if(sidebar.classList.contains("sidebar")){
-            sidebar.classList.remove("sidebar");
-            background.classList.remove("no_background");
-        } else {
-            sidebar.classList.add("sidebar");
-            background.classList.add("no_background")
-        }
+        sidebar.classList.toggle("sidebar");
+        background.classList.toggle("no_background");
     })
 })
